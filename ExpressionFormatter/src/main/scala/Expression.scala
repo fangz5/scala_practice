a35abf0a48b8846720b28da3b815a704db2ffb85
Fang Zhang <fang@levyx.com>
2017-07-06 11:39:55 -0700

    cleaner code

--- ExpressionFormatter/src/main/scala/Expression.scala
+++ ExpressionFormatter/src/main/scala/Expression.scala
@@ -1,11 +1,11 @@
 /**
   * Created by fang on 7/6/17.
   */
-abstract class OperatorType
-object Plus extends OperatorType
-object Minus extends OperatorType
-object Multiply extends OperatorType
-object Divide extends OperatorType
+abstract case class OperatorType(sign: Char)
+object Plus extends OperatorType('+')
+object Minus extends OperatorType('-')
+object Multiply extends OperatorType('*')
+object Divide extends OperatorType('/')
 
 sealed abstract class Expression {
   def layout: Layout
@@ -20,20 +20,9 @@ case class Id(name: String) extends Expression {
   override def layout: Layout = new Layout(Array(name))
 }
 
-case class BinaryOperator(op: OperatorType, left: Expression, right: Expression)
-  extends Expression {
-  override def layout: Layout = {
-    val opChar = op match {
-      case Plus     => '+'
-      case Minus    => '-'
-      case Multiply => '*'
-      case Divide   => '/'
-    }
-
-    if (op == Divide) {
-      left.layout.topOn(right.layout)
-    } else {
-      left.layout.appendedWith(new Layout(Array(s" $opChar "))).appendedWith(right.layout)
-    }
+case class BinaryOperator(op: OperatorType, l: Expression, r: Expression) extends Expression {
+  override def layout: Layout = op match {
+    case Divide => l.layout.topOn(r.layout)
+    case _      => l.layout.appendedWith(new Layout(Array(s" ${op.sign} "))).appendedWith(r.layout)
   }
 }
